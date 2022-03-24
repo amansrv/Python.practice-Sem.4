09fa7a0cd2fbef17f78c5bd10f9baa7e6da06fb3
Aman Srivastava <89743268+amansrv@users.noreply.github.com>
2022-03-24 14:30:50 +0530

    Create 10.py

---
+++ Class-10/10.py
@@ -0,0 +1,43 @@
+que-1
+There are 21 matchsticks and 2 players:
+    player 1 is the user
+    player 2 is the computer
+    a player can pick 1,2,3 or 4 matchsticks.
+    whoever is forced to pick up the last matchstick loses the game.
+    computer should always win.
+    
+    
+    sample input:
+    (1 1 1 1)  (1 1 1)  (1 1)  (1)  1 1 1 1 1 1 1 1 1 1 1 
+    
+    
+
+que-2
+# matrix multiplication
+a=[[1,2,3],[4,5,6],[7,8,9][1,3,5]]
+b=[[2,3],[5,6],[8,9]]    
+for i in a:
+   print(i)
+print i in b:
+    print (i):
+        
+[1,2,3]
+[4,5,6]
+[7,8,9]
+[1,3,5]
+
+[2,3]
+[5,6]
+[8,9]
+
+
+dry run:
+    1 2 3        2 3 
+    4 5 6        5 6
+    7 8 9        8 9 (3*2)   (4*2)    
+    1 3 5 (4*3)  
+    
+    
+    first loop for row count
+    second loop for col count
+    third loop for multiplication
