1d2c0e321cae19054ca3767b55db29fffbd9170d
Aman Srivastava <89743268+amansrv@users.noreply.github.com>
2022-03-30 13:39:34 +0530

    Update 14.py

--- Class-14/14.py
+++ Class-14/14.py
@@ -1,1 +1,16 @@
-
+class Distance:
+    __feet=0
+    __inches=0.0
+    def set(self,feet,inches):
+        self.__feet=feet
+        self.__inches=inches
+        
+    def show(self):
+        print(self.__feet,"'",self.__inches,'"')
+    def input1(self):
+        self.__feet=int(input("enter feet: "))
+        self.__inches=float(input("enter inches:"))
+        
+    d1=Distance()
+    d1.set(5,7.5)
+    d2=distance()
